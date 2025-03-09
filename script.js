baa5d8a08b1885a4a1776213dbd90b4af8831fa4
edenstern <eden74500@gmail.com>
2025-03-09 17:33:15 +0200

    הוספת קובץ JavaScript עם תמיכה בעברית

---
+++ script.js
@@ -0,0 +1,13 @@
+// קובץ JavaScript בסיסי עם הערות בעברית
+
+/**
+ * פונקציה שמציגה הודעת ברכה בעברית
+ * @param {string} name - השם שיוצג בהודעה
+ * @returns {string} - הודעת הברכה המלאה
+ */
+function sayHello(name) {
+  return `שלום ${name}, ברוך הבא לפרויקט!`;
+}
+
+// דוגמה לשימוש בפונקציה
+console.log(sayHello('עולם'));
